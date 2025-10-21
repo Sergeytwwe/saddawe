7ac2974169c1ba61d83c933477cf4f3df506fab3
Sergeytwwe <143936720+Sergeytwwe@users.noreply.github.com>
2025-10-21 18:50:10 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+window.CONFIG = {
+    SUPABASE_URL: 'https://llryoduerphxkyrdxlpy.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxscnlvZHVlcnBoeGt5cmR4bHB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MjEyMTUsImV4cCI6MjA3NjM5NzIxNX0.7uZHvljvSOuuMwO8tFPxiJKl_ITLu0xEW1BZ52zxtGM'
+};
